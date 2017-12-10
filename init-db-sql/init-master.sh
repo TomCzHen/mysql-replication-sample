3050857f40379311e65fcdbaadc1156aa8dd6548
Tom CzHen <tom.czhen@gmail.com>
2017-12-10 21:59:16 +0800

    网关ip末位超过1位时的处理

--- init-db-sql/init-master.sh
+++ init-db-sql/init-master.sh
@@ -4,7 +4,7 @@ set -e
 
 # create replication user
 
-mysql_net=$(ip route | awk '$1=="default" {print $3}' | sed "s/\.[0-9]$/.%/g")
+mysql_net=$(ip route | awk '$1=="default" {print $3}' | sed "s/\.[0-9]\+$/.%/g")
 
 MYSQL_PWD=${MYSQL_ROOT_PASSWORD} mysql -u root \
 -e "CREATE USER '${MYSQL_REPLICATION_USER}'@'${mysql_net}' IDENTIFIED BY '${MYSQL_REPLICATION_PASSWORD}'; \
--- init-db-sql/init-node.sh
+++ init-db-sql/init-node.sh
@@ -11,7 +11,7 @@ done
 
 # create replication user
 
-mysql_net=$(ip route | awk '$1=="default" {print $3}' | sed "s/\.[0-9]$/.%/g")
+mysql_net=$(ip route | awk '$1=="default" {print $3}' | sed "s/\.[0-9]\+$/.%/g")
 
 MYSQL_PWD=${MYSQL_ROOT_PASSWORD} mysql -u root \
 -e "CREATE USER '${MYSQL_REPLICATION_USER}'@'${mysql_net}' IDENTIFIED BY '${MYSQL_REPLICATION_PASSWORD}'; \
